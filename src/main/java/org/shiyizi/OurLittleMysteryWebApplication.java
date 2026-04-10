91174ede47e73ebfe50d51dfaea4b9f472797180
shiyizi-china <1287538346@qq.com>
2026-04-10 23:48:12 +0800

    Update OurLittleMysteryWebApplication.java

--- src/main/java/org/shiyizi/OurLittleMysteryWebApplication.java
+++ src/main/java/org/shiyizi/OurLittleMysteryWebApplication.java
@@ -8,7 +8,6 @@ import org.springframework.boot.web.server.servlet.context.ServletComponentScan;
 @SpringBootApplication
 @ServletComponentScan
 @MapperScan("org.shiyizi.mapper")
-@ComponentScan(basePackages = "org.shiyizi")
 public class OurLittleMysteryWebApplication {
 
     public static void main(String[] args) {
